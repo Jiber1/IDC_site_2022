b836f09bb29b6878c96a5bdf2ad63c59fa1133d8
James Berg <djspy3@gmail.com>
2022-02-02 23:26:22 -0600

    navbar prototype finished

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Options } from "./Options";
+import './Navbar.css'
+
+class Navbar extends Component {
+  state = { onClick: false } // On click boolean for animations/icon transitions, and mobile navbar
+
+  handleClick = () => {
+    this.setState({ onClick: !this.state.onClick})
+  }
+//update ul classname with true false states for mobile navbar
+
+
+  render() {
+    return (
+          <nav className='Navbar'>
+            <h1 className='Navbar-logo'>Illinois Design Challenge<i className="fab fa-react"></i></h1>
+                  <ul className='Navbar-menu'>
+                    {Options.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <a className={item.cName} href={item.url}>
+                            {item.title}
+                          </a>
+                        </li>
+                      )
+                    })}
+                  </ul>  
+          </nav>
+
+    ) 
+  }
+}
+  
+
+export default Navbar;
---
+++ src/components/Navbar/Options.js
@@ -0,0 +1,32 @@
+export const Options = [
+    {
+        title: 'Home',
+        url: '#', //placeholder
+        cName: 'Navbar-links'
+    },
+    {
+        title: 'About us',
+        url: '#', //placeholder
+        cName: 'Navbar-links'
+    },
+    {
+        title: 'FAQ',
+        url: '#', //placeholder
+        cName: 'Navbar-links'
+    },
+    {
+        title: 'Events',
+        url: '#', //placeholder
+        cName: 'Navbar-links'
+    },
+    {
+        title: 'Register',
+        url: '#', //placeholder
+        cName: 'Navbar-links'
+    },
+    //{ //Mobile placeholder
+        //title: 'Register',
+        //url: '#', //placeholder
+       // cName: 'Navbar-links-mobile'
+    //},
+]
