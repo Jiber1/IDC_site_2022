df902063dd533bd832a143121ab8dd6d547524cc
James Berg <djspy3@gmail.com>
2022-04-14 13:32:19 -0500

    updated events

--- src/components/pages/Landingpage/Events.js
+++ src/components/pages/Landingpage/Events.js
@@ -3,38 +3,39 @@ export const Events_One = [
         title: 'Opening Ceremony',
         date: 'April 14, 2022 | 5:00 PM CT',
         place: 'SCD Sunset Studio Room 1050',
-        description: 'Presented by IDC Co-Directors',
+        description: 'by IDC Co-Directors x Brunswick',
+        tag: 'day1'
+    },
+    {
+        title: 'Design Crash Course',
+        date: 'April 14, 2022 | 9:00 PM CT',
+        place: 'SCD Room 1000',
+        description: 'Presented by IDC Staff',
         tag: 'day1'
     }
+
 ]
 
 export const Events_Two = [
     {
-        title: 'Workshops & Speakers',
-        date: 'April 15, 2022 | 2:00 PM CT',
-        place: 'Siebel Center for Design',
-        description: 'Room 1002',
-        tag: 'day2'
-    },
-    {
-        title: 'Workshops & Speakers',
+        title: 'In-Person Project Help',
         date: 'April 15, 2022 | 3:00 PM CT',
-        place: 'Siebel Center for Design',
-        description: 'Room 1002',
+        place: 'SCD Room 1002',
+        description: 'by IDC staff',
         tag: 'day2'
     },
     {
-        title: 'Design Ideation Workshop',
+        title: 'Imagining the Future',
         date: 'April 15, 2022 | 4:00 PM CT',
-        place: 'Siebel Center for Design',
-        description: 'Room 1000 & 1002',
+        place: 'SCD Room 1000 & 1002',
+        description: 'by Prof. Suresh Sethi',
         tag: 'day2'
     },
     {
-        title: 'Workshops & Speakers',
+        title: 'In-Person Project Help',
         date: 'April 15, 2022 | 5:00 PM CT',
-        place: 'Siebel Center for Design',
-        description: 'Room 1000 & 1002',
+        place: 'SCD Room 1000 & 1002',
+        description: 'by IDC Staff',
         tag: 'day2'
     },
     {
@@ -50,38 +51,31 @@ export const Events_Two = [
         place: 'SCD Room 1000 & 1002',
         description: 'By David Parker (iRobotics)',
         tag: 'day2'
+    },
+    {
+        title: 'Movie Night Social',
+        date: 'April 15, 2022 | 10:00 PM CT',
+        place: 'Online Event',
+        description: '',
+        tag: 'day2'
     }
 ]
 
 export const Events_Three = [
     {
-        title: 'Keynote Presentation',
+        title: 'In-Person Project Help',
         date: 'April 16, 2022 | 1:00 PM CT',
-        place: 'Siebel Center for Design',
-        description: 'Sunset Studio Room 1050',
+        place: 'SCD Sunset Studio Room 1050',
+        description: 'by IDC Staff',
         tag: 'day3'
     },
     {
-        title: 'Workshops & Speakers',
+        title: 'Practical Fluid System Design Considerations',
         date: 'April 16, 2022 | 2:00 PM CT',
-        place: 'Online Event',
-        description: 'TBD',
+        place: 'SCD Room 1000 & 1002',
+        description: 'by Chad Peterson',
         tag: 'day3'
     },
-    {
-        title: 'Workshops & Speakers',
-        date: 'April 16, 2022 | 3:00 PM CT',
-        place: 'Online Event',
-        description: 'TBD',
-        tag: 'day3'
-    },
-    {
-        title: 'Workshops & Speakers',
-        date: 'April 16, 2022 | 4:00 PM CT',
-        place: 'Online Event',
-        description: 'TBD',
-        tag: 'day3'
-    }
 ]
 
 export const Events_Four = [
@@ -93,10 +87,10 @@ export const Events_Four = [
         tag: 'day4'
     },
     {
-        title: 'Workshops & Speakers',
+        title: 'In-Person Project Help',
         date: 'April 17, 2022 | 11:00 AM CT',
-        place: 'Siebel Center for Design',
-        description: 'Room 1002',
+        place: 'SCD Room 1002',
+        description: 'by IDC Staff',
         tag: 'day4'
     },
     {
@@ -104,6 +98,13 @@ export const Events_Four = [
         date: 'April 17, 2022 | 2:00 PM CT',
         place: 'Online Event',
         description: 'Presented by IDC Staff',
+        tag: 'day4'
+    },
+    {
+        title: 'Submission Deadline',
+        date: 'April 17, 2022 | 5:00 PM CT',
+        place: 'DevPost',
+        description: '',
         tag: 'day4'
     },
     {
